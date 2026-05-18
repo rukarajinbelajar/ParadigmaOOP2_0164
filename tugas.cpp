4380d37109460908063742a638df3bcf9a174be9
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-18 17:15:31 +0700

    membuat kelas rekening syariah yang menghubungkan ke public rekening

--- tugas.cpp
+++ tugas.cpp
@@ -6,3 +6,22 @@ public:
     virtual void potonganAdmin() = 0;
     virtual ~RekeningBank () {}
 };
+
+class RekeningSyariah : public RekeningBank {
+private:
+    string nama;
+    double saldo;
+public:
+    void setRekening(string n, double s){
+        nama = n;
+        saldo = s;
+    }
+
+    void potonganAdmin() override {
+        cout << "Nama   : " << nama << endl;
+        cout << "Tipe   : Syariah" << endl;
+        cout << "Admin  : Bebas Biaya Admin" << endl;
+        cout << "Saldo  : " << saldo << endl;
+        cout << "____________________" << endl;
+    }
+};
