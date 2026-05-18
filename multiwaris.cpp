56ccebd73b49ee9f7d793b4530c29435966a7e54
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-18 14:55:58 +0700

    membuat program dalam akses public

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -11,4 +11,11 @@ public:
     }
 };
 
-class pekerja : virtual public orang
+class pekerja : virtual public orang {
+public:
+    pekerja(int pUmur) :
+        orang(pUmur)
+    {
+        cout << "pekerja dibuat\n" << endl;
+    }
+};
