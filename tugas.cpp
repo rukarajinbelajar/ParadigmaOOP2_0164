62b6e40226a3df0be731d7f468f046f97b48fa0d
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-18 17:20:38 +0700

    membuat kelas rekening konvensional yang menghubungkan public rekeningbank

--- tugas.cpp
+++ tugas.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class RekeningBank {
 public:
-    virtual void potonganAdmin() = 0;
+    virtual void potongAdmin() = 0;
     virtual ~RekeningBank () {}
 };
 
@@ -17,11 +17,29 @@ public:
         saldo = s;
     }
 
-    void potonganAdmin() override {
+    void potongAdmin() override {
         cout << "Nama   : " << nama << endl;
         cout << "Tipe   : Syariah" << endl;
         cout << "Admin  : Bebas Biaya Admin" << endl;
         cout << "Saldo  : " << saldo << endl;
         cout << "____________________" << endl;
+    }
+};
+
+class rekeningKonvensial : public RekeningBank {
+private:
+    string nama;
+    double saldo;
+public:
+    void setRekening(string n, double s){
+        nama = n;
+        saldo = s;
+    }
+    void potongAdmin() override {
+        saldo -= 15000;
+        cout << "Nama   : " << nama << endl;
+        cout << "Tipe   : Konvensional " << endl;
+        cout << "Admin  : Rp 15.000 dipotong " << endl;
+        cout << "Saldo  : Rp " << saldo << endl;
     }
 };
