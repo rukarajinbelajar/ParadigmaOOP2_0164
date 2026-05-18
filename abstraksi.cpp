1b73a89d7a28e5ffbe09aa3a2f62a3ff59246095
rukarajinbelajar <maar1101dnd@gmail.com>
2026-05-18 14:27:50 +0700

    memberi nilai pada x dan y

--- abstraksi.cpp
+++ abstraksi.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 
 class AbstraksiKlas {
-    
+private: string x, y;
+
+public:
+    // method untuk mengisi nilai 
+    // private member
+    void seTXY(string a, string b){
+        x = a;
+        y = b;
+    }
 }
